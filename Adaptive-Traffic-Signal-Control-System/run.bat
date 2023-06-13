89bdff546dcd4c4b5ebd91640c4112eeb0bcc334
Aarush Mathur <aarushmathur.26may@gmail.com>
2023-06-13 13:52:39 -0400

    Final Commit

---
+++ Adaptive-Traffic-Signal-Control-System/run.bat
@@ -0,0 +1,7 @@
+
+python multithreading.py "/videos/1.mp4" >> out.txt
+python multithreading.py "/videos/2.mp4" >> out.txt
+python multithreading.py "/videos/3.mp4" >> out.txt
+python multithreading.py "/videos/4.mp4" >> out.txt
+python program.py
+Pause
